c1a5fdbbe8d19ef0c44d65b3b29c3d7d964d374e
sakurae1203 <ibuki.m.3030@gmail.com>
2024-01-18 15:19:05 +0900

    出退勤に登録機能完成

--- Sotusei/src/main/java/com/example/demo/controller/SyuttaikinController.java
+++ Sotusei/src/main/java/com/example/demo/controller/SyuttaikinController.java
@@ -29,6 +29,13 @@ public class SyuttaikinController {
 	LocalDate day = LocalDate.now();
 
 	List<String> ID, NAME, SYUKKIN, TAIKIN, BREAKST, BREAKEN, OVER = new ArrayList<String>();
+	
+	List<Map<String, Object>> resultList = jdbcTemplate.queryForList("SELECT CURDATE();");
+	String d = resultList.get(0).get("CURDATE()").toString();
+	//userIDの引継ぎ方がわかり次第修正
+	//String x = (String) session.getAttribute("userID");
+	String x = "12345";
+	String z = x + d;
 
 	int oversum = 0;
 	// 終了する時刻を指定
@@ -80,15 +87,10 @@ public class SyuttaikinController {
 	userIDと年月日を合わせて登録し出勤を一日一回までにする。*/
 	@RequestMapping(path = "/syuttaikin", params = "syukkin", method = RequestMethod.POST)
 	public String syukkin(Model model, HttpSession session) {
-
-		List<Map<String, Object>> resultList = jdbcTemplate.queryForList("SELECT CURDATE();");
-		String d = resultList.get(0).get("CURDATE()").toString();
-		//userIDの引継ぎ方がわかり次第修正
-		//String x = (String) session.getAttribute("userID");
-		String x = "12345";
-		String z = x + d;
+		
 		jdbcTemplate.update("INSERT INTO 出退勤 (userID,date,wortime) VALUES(?,?,CURTIME());", x, z);
-
+		jdbcTemplate.update("INSERT INTO 残業時間 (date) VALUES(?);", z);
+		
 		return "syuttaikin";
 
 	}
@@ -97,24 +99,26 @@ public class SyuttaikinController {
 	@RequestMapping(path = "/syuttaikin", params = "taikin", method = RequestMethod.POST)
 	public String taikin(Model model) throws ParseException {
 
-		List<Map<String, Object>> resultList = jdbcTemplate.queryForList("SELECT CURDATE();");
-		//String x = (String) session.getAttribute("userID");
-		String d = resultList.get(0).get("CURDATE()").toString();
-		String x = "12345";
-		String z = x + d;
-		String targetTimeStr = "10:55:00";
+		String targetTimeStr = "10:29:00";
 
 		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 		java.util.Date targetTime = sdf.parse(targetTimeStr);
+
 		//17時から退勤時間までの時差を計算する
 		if (isTimeAfterTarget(targetTime)) {
 			LocalDateTime nowDate = LocalDateTime.now();
-			String stTime = "10:55:00";
-			String endTime = nowDate.toString();
+			DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+			String formatNowDate = dtf1.format(nowDate);
+			String stTime = "10:29:00";
+			String endTime = formatNowDate.toString();
 			java.util.Date stDate = sdf.parse(stTime);
 			java.util.Date endDate = sdf.parse(endTime);
-			long zisa = calculateTimeDifference(stDate, endDate);
-			jdbcTemplate.update("UPDATE 出退勤 SET closetime = CURTIME(), overtime = ? WHERE date = ?;", zisa, z);
+			int zisa = (int) calculateTimeDifference(stDate, endDate);
+			String lagTime = convertMinutesToTime(zisa);
+			System.out.println(lagTime);
+			//Date lag = new SimpleDateFormat("hh:mm:ss").parse(lagTime);
+			jdbcTemplate.update("UPDATE 出退勤 SET closetime = CURTIME(), overtime = ? WHERE date = ?;", lagTime, z);
+			jdbcTemplate.update("UPDATE 残業時間 SET day = ? WHERE date = ?;", lagTime, z);
 		} else {
 			jdbcTemplate.update("UPDATE 出退勤 SET closetime = CURTIME(), overtime = 0 WHERE date = ?;", z);
 		}
@@ -127,11 +131,6 @@ public class SyuttaikinController {
 	@RequestMapping(path = "/syuttaikin", params = "kaisi", method = RequestMethod.POST)
 	public String kaisi(Model model) {
 
-		List<Map<String, Object>> resultList = jdbcTemplate.queryForList("SELECT CURDATE();");
-		//String x = (String) session.getAttribute("userID");
-		String d = resultList.get(0).get("CURDATE()").toString();
-		String x = "12345";
-		String z = x + d;
 		jdbcTemplate.update("UPDATE 出退勤 SET breakbegins = CURTIME() WHERE date = ?;", z);
 
 		return "syuttaikin";
@@ -141,12 +140,7 @@ public class SyuttaikinController {
 	//休憩終了時間登録メソッド
 	@RequestMapping(path = "/syuttaikin", params = "syuuryou", method = RequestMethod.POST)
 	public String syuuryou(Model model) {
-
-		List<Map<String, Object>> resultList = jdbcTemplate.queryForList("SELECT CURDATE();");
-		//String x = (String) session.getAttribute("userID");
-		String d = resultList.get(0).get("CURDATE()").toString();
-		String x = "12345";
-		String z = x + d;
+		
 		LocalTime nowtime = LocalTime.now();
 		if (nowtime.equals("17:00:00")) {
 
@@ -167,7 +161,7 @@ public class SyuttaikinController {
 	}
 
 	//時間差計算メソッド
-	public static long calculateTimeDifference(java.util.Date stDate, java.util.Date endDate) {
+	public static double calculateTimeDifference(java.util.Date stDate, java.util.Date endDate) {
 		LocalTime stLocalTime = LocalTime.ofInstant(stDate.toInstant(), java.time.ZoneId.systemDefault());
 		LocalTime endLocalTime = LocalTime.ofInstant(endDate.toInstant(), java.time.ZoneId.systemDefault());
 
@@ -176,7 +170,20 @@ public class SyuttaikinController {
 			endLocalTime = endLocalTime.plusHours(24);
 		}
 
-		Duration duration = Duration.between(stLocalTime, endLocalTime);
-		return duration.toMinutes();
+	    Duration duration = Duration.between(stLocalTime, endLocalTime);
+	    double secondsDifference = duration.toMillis() / 1000.0;
+
+	    //return minutesDifference; // 分単位での差
+	    return secondsDifference; // 秒単位での差
+	}
+
+	public static String convertMinutesToTime(long zisa) {
+		// 分を時間、分、秒に変換
+		long hou = zisa / 3600;
+		long min = zisa % 3600 / 60;
+		long sec = zisa % 60;
+
+		// 時刻形式に変換して文字列として返す
+		return String.format("%02d:%02d:%02d", hou,min,sec);
 	}
 }
